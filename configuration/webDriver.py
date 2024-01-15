00a1255a603adbeae19323eed3669f4480ad4936
doyeon <dykim@suprema.co.kr>
2024-01-15 11:16:53 +0900

    Webdriver 모듈화

--- configuration/webDriver.py
+++ configuration/webDriver.py
@@ -1,14 +1,22 @@
+import json
+import os
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 
+class AppiumConfig:
+    @staticmethod
+    def get_driver():
+        # 현재 스크립트의 디렉토리 경로를 얻음
+        current_dir = os.path.dirname(os.path.realpath(__file__))
 
-desired_cap = {
-    "appium:deviceName": "R39M30571MZ",
-    "platformName": "Android",
-    "appium:appPackage": "com.suprema.moon",
-    "appium:appActivity": "com.suprema.moon.MainActivity"
-}
+        # 상대 경로를 사용하여 capability.json 파일의 전체 경로를 구성
+        config_path = os.path.join(current_dir, 'capability.json')
 
-def mainApp():
-    wd = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
-    wd.implicitly_wait(10)
-    return  wd
+        with open(config_path) as config_file:
+            config = json.load(config_file)
+
+        options = UiAutomator2Options()
+        for key, value in config.items():
+            setattr(options, key, value)
+
+        return webdriver.Remote("http://localhost:4723/wd/hub", options=options)
